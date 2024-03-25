02ad9284dfc63f7ef82cc7952a2a5b7e280d17ab
zyon <zyonzarhaf@gmail.com>
2024-03-25 20:03:18 -0300

    refactor: remove product page

--- app/products/[product]/page.jsx
+++
@@ -1,9 +0,0 @@
-import Product from '../../components/Product';
-
-export default function ProductPage({ params }) {
-  return (
-    <section>
-      <Product _id={params.product}/>
-    </section>
-  );
-}
