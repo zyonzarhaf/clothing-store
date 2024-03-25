2d08cb1c445434567b239e41192cb803ee41d175
zyon <zyonzarhaf@gmail.com>
2024-03-25 19:31:47 -0300

    refactor: fetch all products

--- app/components/FeaturedProducts.jsx
+++ app/components/FeaturedProducts.jsx
@@ -1,9 +1,18 @@
 import Products from './Products';
 
-export default function FeaturedProducts() {
+async function fetchProducts() {
+  const response = await fetch('http://localhost:8000/api/products');
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const data = await response.json();
+  return data;
+}
+
+export default async function FeaturedProducts() {
+  const products = await fetchProducts();
+
   return (
     <section>
-        <Products />
+        <Products products={products} />
     </section>
   )
 }
