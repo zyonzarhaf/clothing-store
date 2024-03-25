18ae4da94c708d91931a5c1e2b87ed50bca641d4
zyon <zyonzarhaf@gmail.com>
2024-03-25 20:05:54 -0300

    refactor: prepare to render dropdown menu items

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -7,6 +7,83 @@ import { CiMenuBurger } from 'react-icons/ci';
 
 import styles from './Navbar.module.css';
 
+const menuItems = [
+  {
+    title: "Home",
+    route: "/"
+  },
+  {
+    title: "About",
+    route: "/about"
+  },
+  {
+    title: "Women",
+    children: [
+      {
+        title: "all",
+        route: "/products/women/all"
+      },
+      {
+        title: "coats-and-jackets",
+        route: "/products/women/coats-and-jackets"
+      },
+      {
+        title: "t-shirts",
+        route: "/products/women/t-shirts"
+      },
+      {
+        title: "pants",
+        route: "/products/women/pants"
+      },
+      {
+        title: "underwear",
+        route: "/products/women/pants"
+      },
+      {
+        title: "shoes",
+        route: "/products/women/shoes"
+      }
+    ]
+  },
+  {
+    title: "Men",
+    children: [
+      {
+        title: "all",
+        route: "/products/men/all"
+      },
+      {
+        title: "coats-and-jackets",
+        route: "/products/men/coats-and-jackets"
+      },
+      {
+        title: "t-shirts",
+        route: "/products/men/t-shirts"
+      },
+      {
+        title: "pants",
+        route: "/products/men/pants"
+      },
+      {
+        title: "underwear",
+        route: "/products/men/underwear"
+      },
+      {
+        title: "shoes",
+        route: "/products/men/shoes"
+      }
+    ]
+  },
+  {
+    title: "Login",
+    route: "/login"
+  },
+  {
+    title: "Register",
+    route: "/register"
+  }
+];
+
 export default function Navbar() {
   const pathName = usePathname();
 
